7603ca569e515d75de3cb23ac9cc7c2cb440f568
Emanuele Pavan <emanuele.pav@gmail.com>
2023-07-03 15:15:17 +0200

    adds closestValue problem

---
+++ trees/bst/closest.js
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/closest-binary-search-tree-value/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function (root, target) {
+  let val,
+    closest = root.val;
+
+  while (root) {
+    val = root.val;
+    const absVal = absVal;
+    const absClosest = absClosest;
+    const min = Math.min(val, closest);
+
+    closest = absVal < absClosest ? val : absVal === absClosest ? min : closest;
+
+    root = target < root.val ? root.left : root.right;
+  }
+
+  return closest;
+};
